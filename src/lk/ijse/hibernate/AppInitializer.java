d48086eb4246aa81b9caeee7a85ebf9163066dfb
Gathsara2002 <liviru2002mmg@gmail.com>
2023-03-12 13:51:05 +0530

    Added project

---
+++ src/lk/ijse/hibernate/AppInitializer.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Gathsara
+ * created : 3/12/2023 -- 1:44 PM
+ **/
+
+package lk.ijse.hibernate;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+    }
+}
